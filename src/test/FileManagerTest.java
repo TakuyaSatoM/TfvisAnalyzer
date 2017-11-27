47dc432f38829d957a3b759ba0389cbfc6c996da
sato <sato@Neptune>
2017-11-27 18:41:17 +0900

    FileManagerテストケースを追加

--- src/test/FileManagerTest.java
+++ src/test/FileManagerTest.java
@@ -3,6 +3,7 @@ package test;
 import static org.assertj.core.api.Assertions.*;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
 import org.junit.Test;
@@ -12,23 +13,45 @@ import tfvis.FileManager;
 public class FileManagerTest {
 
 	@Test
-	public void outputフォルダのファイルを削除する() {
+	public void outputフォルダのファイルを削除する() throws IOException {
 		// Arrange
 		int expected = 0;
+		File targetFolder = new File("output");
+		FileManager fileManager = new FileManager();
+		ArrayList<File> classFiles = new ArrayList<File>();
 
 		// Act
-		FileManager fileManager = new FileManager();
-		File targetFolder = new File("output");
 		fileManager.clearDir(targetFolder);
-
-		ArrayList<File> classFiles = new ArrayList<File>();
 		classFiles = fileManager.getClassFile(targetFolder);
 
 		int actual = classFiles.size();
 
 		// Assert
 		assertThat(actual).isEqualTo(expected);
+	}
 
+	@Test
+	public void デバッグターゲットクラスファイルを取得する() {
+		// Arrange
+		String expected = "Main.class";
+		File targetFolder = new File("target/bin");
+		FileManager fileManager = new FileManager();
+		fileManager.clearDir(targetFolder);
+		try {
+			targetFolder.createTempFile("Main", "class");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		ArrayList<File> classFiles = new ArrayList<File>();
+
+		// Act
+		classFiles = fileManager.getClassFile(targetFolder);
+
+		// Assert
+		for (File file : classFiles) {
+			assertThat(file.getName()).isEqualTo(expected);
+		}
 	}
 
 }
